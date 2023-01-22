aae0a78faef6f12eb6cada5b355fd8169e17fb5f
Paulina F <faltynp@gmail.com>
2023-01-22 18:39:20 +0100

    dodanie funkcjonalnosci saldo i przeglad

--- app-magazyn.py
+++ app-magazyn.py
@@ -1,5 +1,7 @@
-stan_konta = 100
-funkcje_programu = """Dostępne funkcje:
+funkcje_programu = """
+########################
+### DOSTĘPNE FUNKCJE ###
+########################
 saldo - Pobiera lub dodaje kwotę do konta.
 sprzedaż - Pobiera nazwę produktu, cenę oraz liczbę sztuk.
 zakup - Pobiera nazwę produktu, cenę oraz liczbę sztuk.
@@ -7,53 +9,91 @@ konto - Wyświetla stan konta.
 lista - Wyświetla całkowity stan magazynu wraz z cenami produktów i ich ilością.
 magazyn - Wyświetla stan magazynu dla konkretnego produktu.
 przegląd - Wyświetla wszystkie wprowadzone akcje.
-koniec - Aplikacja kończy działanie."""
-print(funkcje_programu)
-polecenie = input("Wpisz wybraną opcję: ")
+koniec - Aplikacja kończy działanie.
+######################
+"""
+mozliwe_akcje = ("saldo", "sprzedaż", "zakup", "konto", "lista", "magazyn", "przegląd", "koniec")
+akcje = []
+stany_magazynowe = {}
+ceny_produktow = {}
+stan_konta = 0
+menu = True
 while True:
+    print(funkcje_programu)
+    polecenie = input("""Wpisz wybrane polecenie: """)
+    if polecenie not in mozliwe_akcje:
+        menu = False
+        print(f"""Polecenie {polecenie} jest nieprawdiłowe.
+        
+Wpisz polecenie ponownie: """)
+        menu += 1
     if polecenie == "saldo":
-        decyzja_dla_salda = int(input("""Jeśli chcesz pobrać pieniądze z konta naciśnij 1. 
-Jeśli chcesz dodać naciśnij 2.
-Aby powrócić naciśnij 3."""))
-        if decyzja_dla_salda == 1:
+        menu = False
+        akcje.append(polecenie)
+        decyzja_dla_salda = input("""Jeśli chcesz POBRAĆ środki z konta, napisz \"pobierz\". 
+
+Jeśli chcesz DODAĆ środki do konta, napisz \"dodaj\".""")
+        if decyzja_dla_salda == "pobierz":
+            akcje.append(decyzja_dla_salda)
             kwota_do_pobrania = float(input("Wpisz kwotę do pobrania z konta: "))
             stan_konta -= kwota_do_pobrania
             if stan_konta < 0:
                 stan_konta += kwota_do_pobrania
-                print(f"""Uwaga, stan konta nie może być mniejszy niż 0 PLN. 
-Wybierz kwotę mniejszą niż {stan_konta} PLN.""")
-                decyzja_dla_salda = 1
-            else:
-                print(f"""Pobrano {kwota_do_pobrania} PLN. 
-Pozostało na koncie: {stan_konta} PLN. 
+                print(f"""Uwaga! Stan konta po tej operacji nie może być mniejszy niż 0 PLN. 
+Wpisz kwotę mniejszą niż {stan_konta} PLN:
 """)
-                print(funkcje_programu)
-        if decyzja_dla_salda == 2:
+                decyzja_dla_salda = "pobierz"
+            else:
+                akcje.append(kwota_do_pobrania)
+                print(f"""Pobrano {kwota_do_pobrania} PLN. 
+Na koncie pozostało {stan_konta} PLN. 
+""")
+                menu = True
+        if decyzja_dla_salda == "dodaj":
+            akcje.append(decyzja_dla_salda)
             kwota_do_dodania = float(input("Wpisz kwotę do dodania: "))
             stan_konta += kwota_do_dodania
-            print(f"""Dodano {kwota_do_dodania} PLN."
+            akcje.append(kwota_do_dodania)
+            print(f"""Dodano {kwota_do_dodania} PLN.
 Aktualny stan konta to {stan_konta} PLN. """)
-            print(funkcje_programu)
-        if decyzja_dla_salda == 3:
-            print(funkcje_programu)
+            menu = True
     if polecenie == "sprzedaż":
-       nazwa_produktu = input("Podaj nazwę produktu. ")
-       cena = float(input("Podaj cenę produktu. "))
-       liczba_sztuk = int(input("Podaj ilość. "))
+        menu = False
+        nazwa_produktu = input("Podaj nazwę produktu. ")
+        cena = float(input("Podaj cenę produktu. "))
+        liczba_sztuk = int(input("Podaj ilość. "))
+        print(f"""Dodano produktów: {liczba_sztuk} """)
+        menu += 1
     if polecenie == "zakup":
-       nazwa_produktu = input("Podaj nazwę produktu. ")
-       cena = float(input("Podaj cenę produktu. "))
-       liczba_sztuk = int(input("Podaj ilość. "))
+        menu = False
+        nazwa_produktu = input("Podaj nazwę produktu. ")
+        cena = float(input("Podaj cenę produktu. "))
+        liczba_sztuk = int(input("Podaj ilość. "))
+        print(f"""Dodano {nazwa_produktu} w ilosci {liczba_sztuk} . 
+Cena za sztuke to {cena}. """)
+        menu += 1
     if polecenie == "konto":
-        print(f"Stan konta wynosi {stan_konta} PLN")
+        menu = False
+        print(f"Aktualny stan konta wynosi {stan_konta} PLN.")
+        menu += 1
     if polecenie == "lista":
-        print("tbd")
-      #TODO napisac petle
+        menu = False
+        print("TBD")
+        menu += 1
+        #TODO napisac to
     if polecenie == "magazyn":
-        print("tbd")
-       #TODO napisac petle
+        menu = False
+        print(stany_magazynowe)
+        menu += 1
+        #TODO napisac to
     if polecenie == "przegląd":
-        print("tbd")
-     #TODO napisac petle
+        menu = False
+        print(f"""Wykonano następujące akcje: 
+{akcje} """)
+        menu += 1
     if polecenie == "koniec":
-       print("Koniec pracy programu.")
+       print("Kończę pracę programu.")
+       break
+
+
+
