e67aeae708939c77244e7481b1f219eaf508acea
Paulina F <faltynp@gmail.com>
2023-01-27 22:52:32 +0100

    zakup i sprzedaz dodane

--- app-magazyn.py
+++ app-magazyn.py
@@ -1,4 +1,3 @@
-# Setup
 funkcje_programu = """
 ########################
 ### DOSTĘPNE FUNKCJE ###
@@ -18,98 +17,101 @@ akcje = []
 stan_konta = 0
 magazyn = {}
 ilosc_i_cena = []
-# magazyn = {
+# Magazyn = {
 #     "produkt_1" : [ilosc, cena],
 #     "produkt_2" : [ilosc, cena] }
-menu = True
-# Start
 while True:
     print(funkcje_programu)
-    polecenie = input(""">>> Wpisz wybrane polecenie: """)
+    polecenie = input("Wpisz wybrane polecenie: ")
     if polecenie not in mozliwe_akcje:
-        menu = False
-        print(f"""Polecenie {polecenie} jest nieprawdiłowe.
->>> Wpisz polecenie ponownie: """)
-        menu += 1
-    if polecenie == "saldo":
-        menu = False
         akcje.append(polecenie)
-        decyzja_dla_salda = input(""">>> Jeśli chcesz POBRAĆ środki z konta, napisz \"pobierz\". 
->>> Jeśli chcesz DODAĆ środki do konta, napisz \"dodaj\".""")
+        print(f"""Polecenie {polecenie} jest nieprawdiłowe. 
+Wpisz polecenie ponownie: """)
+    elif polecenie == "saldo":
+        akcje.append(polecenie)
+        decyzja_dla_salda = input("""Jeśli chcesz POBRAĆ środki z konta, napisz \"pobierz\". 
+Jeśli chcesz DODAĆ środki do konta, napisz \"dodaj\".""")
         if decyzja_dla_salda == "pobierz":
-            kwota_do_pobrania = float(input(">>> Wpisz kwotę do pobrania z konta: "))
+            akcje.append(decyzja_dla_salda)
+            kwota_do_pobrania = float(input("Wpisz kwotę do pobrania z konta: "))
+            akcje.append(kwota_do_pobrania)
             stan_konta -= kwota_do_pobrania
             if stan_konta < 0:
                 stan_konta += kwota_do_pobrania
-                print(f"""Uwaga! Stan konta po tej operacji nie może być mniejszy niż 0 PLN. 
->>> Wpisz kwotę mniejszą niż {stan_konta} PLN: """)
-                decyzja_dla_salda = "pobierz"
+                print(f"""Uwaga! Stan konta po tej operacji nie może być mniejszy lub równy 0 PLN. 
+Wpisz kwotę mniejszą niż {stan_konta} PLN: """)
             else:
-                akcje.append(decyzja_dla_salda)
-                akcje.append(kwota_do_pobrania)
                 print(f"""Pobrano {kwota_do_pobrania} PLN. 
 Na koncie pozostało {stan_konta} PLN. """)
-                menu = True
-        if decyzja_dla_salda == "dodaj":
-            akcje.append(decyzja_dla_salda)
-            kwota_do_dodania = float(input(">>> Wpisz kwotę do dodania: "))
+        elif decyzja_dla_salda == "dodaj":
+            kwota_do_dodania = float(input("Wpisz kwotę do dodania: "))
             stan_konta += kwota_do_dodania
-            akcje.append(kwota_do_dodania)
+            akcje.extend([decyzja_dla_salda, kwota_do_dodania])
             print(f"""Dodano {kwota_do_dodania} PLN.
 Aktualny stan konta to {stan_konta} PLN. """)
-            menu = True
-    if polecenie == "sprzedaż":
-        menu = False
+    elif polecenie == "sprzedaż":
         nazwa_produktu = input(">>> Podaj nazwę produktu: ")
-        liczba_sztuk = int(input(">>> Podaj ilość: "))
-        cena = float(input(">>> Podaj cenę produktu: "))
-        # Produkt musi znajdować się w magazynie.
+        akcje.extend([polecenie, nazwa_produktu])
+        if nazwa_produktu not in magazyn:
+            print(f"""Uwaga, brak produktu: {nazwa_produktu} w magazynie.
+Wybierz ponownie.""")
+        liczba_sztuk = int(input("Podaj ilość: "))
+        cena = float(input("Podaj cenę jednostkową produktu: "))
+        akcje.extend([liczba_sztuk, cena])
         if nazwa_produktu in magazyn:
-            stan_konta =+ liczba_sztuk * cena
-            menu += 1
-    if polecenie == "zakup":
-        menu = False
+            sprawdzenie_dostepnosci = ilosc_i_cena[0] - int(liczba_sztuk)
+            if sprawdzenie_dostepnosci <= 0:
+                print(f"""Uwaga! Za niski stan magazynowy dla produktu {nazwa_produktu}.
+Spróbuj ponownie.""")
+            elif sprawdzenie_dostepnosci > 0:
+                stan_konta += int(liczba_sztuk) * float(cena)
+        elif nazwa_produktu not in magazyn:
+            print(f"""Nie znaleziono produktu {nazwa_produktu} .
+Podany produkt nie znajduje się w magazynie.""")
+    elif polecenie == "zakup":
         nazwa_produktu = input(">>> Podaj nazwę produktu:  ")
         liczba_sztuk = int(input(">>> Podaj ilość: "))
         cena = float(input(">>> Podaj cenę jednostkową produktu: "))
-        stan_konta -= liczba_sztuk * cena
-        if stan_konta < 0:
-            print("""Uwaga! Ujemna wartość konta po zakończeniu tej operacji.
-Operacja niedozwolona.
-Przerywam transakcję. """)
-            stan_konta += liczba_sztuk * cena
-            menu = True
-        else:
-            ilosc_i_cena.extend([liczba_sztuk, cena])
-            magazyn[(str(nazwa_produktu))] = ilosc_i_cena
-            akcje.extend([polecenie, nazwa_produktu, liczba_sztuk, cena])
-            print(f"""Aktualizacja stanu magazynu:
+        akcje.extend([polecenie, nazwa_produktu, liczba_sztuk, cena])
+        sprawdz_stan_konta = stan_konta - int(liczba_sztuk) * float(cena)
+        if sprawdz_stan_konta < 0:
+            print("""Uwaga! Nieprawidłowy stan konta po zakończeniu tej operacji. 
+Operacja niedozwolona. Przerywam akcję. """)
+        elif sprawdz_stan_konta > 0:
+            stan_konta -= int(liczba_sztuk) * float(cena)
+        if nazwa_produktu not in magazyn:
+            ilosc_i_cena = [0, 0]
+            ilosc_i_cena[0] = liczba_sztuk
+            ilosc_i_cena[1] = cena
+            magazyn[str(nazwa_produktu)] = ilosc_i_cena
+            print(f"""Aktualizacja stanu magazynu. Dodano:
 produkt: {nazwa_produktu}
 dodano sztuk: {liczba_sztuk}
-cena jednostkowa: {cena} 
+cena jednostkowa: {cena} PLN
 """)
-            menu = True
-    if polecenie == "konto":
-        menu = False
+        elif nazwa_produktu in magazyn:
+            ilosc_i_cena[0] += int(liczba_sztuk)
+            ilosc_i_cena[1] = float(cena)
+    elif polecenie == "konto":
         print(f"Aktualny stan konta wynosi {stan_konta} PLN.")
-        menu += 1
-    if polecenie == "lista":
-        menu = False
-        print(magazyn)
-        menu += 1
-    if polecenie == "magazyn":
-        menu = False
+    elif polecenie == "lista":
+        caly_magazyn = magazyn
+        print(f"""Stan magazynu (produkt: ilość, cena) to:"
+        {magazyn} """)
+    elif polecenie == "magazyn":
         wybrany_produkt = input("""Podaj nazwę produktu, 
 dla którego chcesz poznać stan magazynowy.""")
-        #TODO reszta
-        menu += 1
-    if polecenie == "przegląd":
-        menu = False
+        if wybrany_produkt not in magazyn:
+            print(f"Błąd. Produkt {wybrany_produkt} nie istnieje.")
+        elif wybrany_produkt in magazyn:
+            stan_umyslu = magazyn[wybrany_produkt]
+            print(f"""Stan magazynowy dla {wybrany_produkt} to: (ilość, cena w PLN): 
+{stan_umyslu} """)
+    elif polecenie == "przegląd":
         wszystkie_akcje = input(f""" Wykonano następującą liczbę operacji: {len(akcje)} .
 Wyświetlić wszystkie akcje? T/N : >>> """)
         if wszystkie_akcje == "T":
             print(akcje)
-            menu = True
         else:
             print("""Aby wyświetlić akcje w wybranym zakresie odpowiedz na 2 pytania: """)
             od = int(input(""">>> OD której operacji rozpocząć? 
@@ -117,10 +119,9 @@ Wpisz wybrany numer operacji: >>> """))
             do = int(input("""Do której operacji wyświetlić akcje?
 Wpisz numer ostatniej żądanej operacji: >>> """))
             print(f"Wybrano zakres od operacji nr {od} do operacji nr {do}.")
-            od -= 1 # bo index zaczyna sie od 0; nie trzeba odejmowac 1 od zmiennej "do", bo index "stop" jest elusive (nie "wlącznie")
+            od -= 1
             print(f"""W danym zakresie wykonano następujące akcje:
 {akcje[od:do]}""")
-            menu += 1
     if polecenie == "koniec":
        print("Kończę pracę programu.")
        break
